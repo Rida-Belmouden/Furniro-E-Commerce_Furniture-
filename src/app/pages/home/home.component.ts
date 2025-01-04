a2959d1671d1e271a13e2bc550454693f4efa59e
r.belmouden <rida@belmouden.com>
2025-01-04 15:09:36 +0100

    Fix Merge branch 'master'

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,17 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { HeaderComponent } from "../../components/header/header.component";
 import { HeroComponent } from '../../components/hero/hero.component';
-import { FooterComponent } from '../../components/footer/footer.component';
-=======
-import { HeaderComponent } from "../../Components/header/header.component";
 import { ProductsComponent } from "../../components/products/products.component";
->>>>>>> 3705d729c185f03c624921c5f3465359fe1aa5e7
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-<<<<<<< HEAD
-  imports: [HeaderComponent, HeroComponent, FooterComponent],
-=======
-  imports: [HeaderComponent, ProductsComponent],
->>>>>>> 3705d729c185f03c624921c5f3465359fe1aa5e7
+  imports: [HeaderComponent, HeroComponent, ProductsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
 
 }
