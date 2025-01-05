aa1353eda56012e57d514f5ae47fba8662c30cd9
r.belmouden <rida@belmouden.com>
2025-01-06 00:13:50 +0100

    ProductsComponent is finished, some CSS improvements made.

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,43 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { KeyValue } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 
-export class ProductsComponent {
-  products = [
-    {
-      "title": "Syltherine",
-      "img": "",
-      "category": "Stylish cafe chair",
-      "realPrice": "3.500.000",
-      "price": "Rp 2.500.00"
-    },
-    {
-      "title": "Leviosa",
-      "img": "",
-      "category": "Stylish cafe chair",
-      "realPrice": "",
-      "price": "Rp 2.500.000"
-    },
-    {
-      "title": "Lolito",
-      "img": "",
-      "category": "Luxury big sofa",
-      "realPrice": "Rp 14.000.000",
-      "price": "Rp 7.000.000"
-    }
-  ]
+export class ProductsComponent{
+  // Tracks the visibility of the Overlay div
+  showOverlay: boolean[] = [false];
 
-  keys(): Array<any> {
-    console.log(Object.entries(this.products))
-    return Object.entries(this.products);
+  private apiUrl = 'http://localhost:4200/db/products.json';
+  private http = inject(HttpClient);
+  products: any[] = [];
+
+  constructor() {
+    this.fetchProducts();
   }
 
+  fetchProducts() {
+    this.http.get(this.apiUrl).subscribe((res) => {
+      this.products = res as any[];
+    });
+  }
 }
 
