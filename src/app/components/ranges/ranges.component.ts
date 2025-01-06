38692b43826304d1f1fff9de166ba95ec65fa497
r.belmouden <rida@belmouden.com>
2025-01-06 23:36:15 +0100

    Add RangesComponent

---
+++ src/app/components/ranges/ranges.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ranges',
+  standalone: true,
+  imports: [],
+  templateUrl: './ranges.component.html',
+  styleUrl: './ranges.component.css'
+})
+export class RangesComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { HeroComponent } from '../../components/hero/hero.component';
 import { ProductsComponent } from "../../components/products/products.component";
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RangesComponent } from "../../components/ranges/ranges.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, ProductsComponent, FooterComponent],
+  imports: [HeaderComponent, HeroComponent, ProductsComponent, FooterComponent, RangesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
